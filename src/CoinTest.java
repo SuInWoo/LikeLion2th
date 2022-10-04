3b021dd71bccffbff1efdc6b57b1792d1dc14b9a
SuInWoo <wbo1026@naver.com>
2022-10-04 14:26:31 +0900

    금액을 받아 화폐건당 몫과 나머지 출력

--- src/CoinTest.java
+++ src/CoinTest.java
@@ -16,22 +16,14 @@ public class CoinTest {
         int curr50 = 50;
         int curr10 = 10;
 
-        System.out.printf("50000원: %d장\n",refund/curr50000);
-        refund = refund%curr50000;
-        System.out.printf("10000원: %d장\n",refund/curr10000);
-        refund = refund%curr10000;
-        System.out.printf("5000원: %d장\n",refund/curr5000);
-        refund = refund%curr5000;
-        System.out.printf("1000원: %d장\n",refund/curr1000);
-        refund = refund%curr1000;
-        System.out.printf("500원: %d장\n",refund/curr500);
-        refund = refund%curr500;
-        System.out.printf("100원: %d장\n",refund/curr100);
-        refund = refund%curr100;
-        System.out.printf("50원: %d장\n",refund/curr50);
-        refund = refund%curr50;
-        System.out.printf("10원: %d장\n",refund/curr10);
-        refund = refund%curr10;
+        System.out.printf("50000원: %d장 나머지: %d\n",refund/curr50000,refund%curr50000);
+        System.out.printf("10000원: %d장 나머지: %d\n",refund/curr10000,refund%curr10000);
+        System.out.printf("5000원: %d장 나머지: %d\n",refund/curr5000,refund%curr5000);
+        System.out.printf("1000원: %d장 나머지: %d\n",refund/curr1000,refund%curr1000);
+        System.out.printf("500원: %d장 나머지: %d\n",refund/curr500,refund%curr500);
+        System.out.printf("100원: %d장 나머지: %d\n",refund/curr100,refund%curr100);
+        System.out.printf("50원: %d장 나머지: %d\n",refund/curr50,refund%curr50);
+        System.out.printf("10원: %d장 나머지: %d\n",refund/curr10,refund%curr10);
 
     }
 }
