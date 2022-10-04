12183cdb46384dd5b4749cfbf65a915fc767fc5a
SuInWoo <wbo1026@naver.com>
2022-10-04 16:44:33 +0900

    코드업 1010번: 입력받은 정수 출력

---
+++ src/codeup/problem1004/Code1010.java
@@ -0,0 +1,12 @@
+package codeup.problem1004;
+
+import java.util.Scanner;
+
+public class Code1010 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+        System.out.printf("%d",num);
+    }
+}
