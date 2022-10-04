f1ede90cd4c1cd1f50756e23a5eb5781cee4990f
SuInWoo <wbo1026@naver.com>
2022-10-04 16:46:38 +0900

    코드업 1011번: 입력받은 문자 출력

---
+++ src/codeup/problem1004/Code1011.java
@@ -0,0 +1,13 @@
+package codeup.problem1004;
+
+import java.util.Scanner;
+
+public class Code1011 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.nextLine();
+
+        System.out.printf("%s",s);
+    }
+}
