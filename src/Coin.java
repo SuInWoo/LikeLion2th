6cb12cc65d25e34102d1de6ff94a3e95a7df815b
SuInWoo <wbo1026@naver.com>
2022-10-04 14:04:32 +0900

    화폐 개수 출력 수정

--- src/Coin.java
+++ src/Coin.java
@@ -7,7 +7,7 @@ public class Coin {
         System.out.println("금액입력: ");
         coin = sc.nextInt();
 
-        int coinArr[] = {50000, 10000, 5000, 1000, 500, 100, 50, 10};
+        int coinArr[] = new int[]{50000, 10000, 5000, 1000, 500, 100, 50, 10};
         int current[] = new int[8];
 
         for (int i = 0; i < 8; i++) {
