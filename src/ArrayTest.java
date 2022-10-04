02de960eb8d93bf64006109e70a100b9d8484467
SuInWoo <wbo1026@naver.com>
2022-10-04 13:19:51 +0900

    자바 배열 1~10까지 저장 후 출력

---
+++ src/ArrayTest.java
@@ -0,0 +1,13 @@
+public class ArrayTest {
+    public static void main(String[] args) {
+        int[] arr = new int[10];
+
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = i+1;
+        }
+
+        for (int a: arr) {
+            System.out.print(a + " ");
+        }
+    }
+}
