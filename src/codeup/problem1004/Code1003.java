239d03db5940af1ec0315844ffe423ea7e96e298
SuInWoo <wbo1026@naver.com>
2022-10-04 16:29:52 +0900

    코드업 1003번: Hello \n World

--- src/codeup/problem1004/Code1003.java
+++ src/codeup/problem1004/Code1003.java
@@ -2,6 +2,6 @@ package codeup.problem1004;
 
 public class Code1003 {
     public static void main(String[] args) {
-        System.out.println("Hello\nWorld");
+        System.out.println("Hello \nWorld");
     }
 }
