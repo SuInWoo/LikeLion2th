e57d9cfd7134afe4be90a00d4d7b9ce73d076ea4
SuInWoo <wbo1026@naver.com>
2022-10-04 16:04:38 +0900

    코드업 1161번: 홀수와 짝수 그리고 더하기 - %를 활용하여 풀이

---
+++ src/course1004/even_odd/EvenPlusOdd.java
@@ -0,0 +1,20 @@
+package course1004.even_odd;
+
+import java.util.Scanner;
+
+public class EvenPlusOdd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+
+        if(num1 % 2 == 0 && num2 % 2 ==0){
+            System.out.println("짝수+짝수=짝수");
+        }else if(num1 % 2 == 0 && num2 % 2 !=0) {
+            System.out.println("짝수+홀수=홀수" );
+        } else if (num1 % 2 != 0 && num2 % 2 ==0) {
+            System.out.println("홀수+짝수=홀수" );
+        }else System.out.println("홀수+홀수=짝수" );
+    }
+}
