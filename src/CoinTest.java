f6b7977aae925dc47cdb43cf200ae347720e39dc
SuInWoo <wbo1026@naver.com>
2022-10-04 14:46:26 +0900

    금액을 받아 화폐건당 몫과 나머지 출력 (refund 추가)

--- src/CoinTest.java
+++ src/CoinTest.java
@@ -17,13 +17,21 @@ public class CoinTest {
         int curr10 = 10;
 
         System.out.printf("50000원: %d장 나머지: %d\n",refund/curr50000,refund%curr50000);
+        refund %= curr50000;
         System.out.printf("10000원: %d장 나머지: %d\n",refund/curr10000,refund%curr10000);
+        refund %= curr10000;
         System.out.printf("5000원: %d장 나머지: %d\n",refund/curr5000,refund%curr5000);
+        refund %= curr5000;
         System.out.printf("1000원: %d장 나머지: %d\n",refund/curr1000,refund%curr1000);
+        refund %= curr1000;
         System.out.printf("500원: %d장 나머지: %d\n",refund/curr500,refund%curr500);
+        refund %= curr500;
         System.out.printf("100원: %d장 나머지: %d\n",refund/curr100,refund%curr100);
+        refund %= curr100;
         System.out.printf("50원: %d장 나머지: %d\n",refund/curr50,refund%curr50);
+        refund %= curr50;
         System.out.printf("10원: %d장 나머지: %d\n",refund/curr10,refund%curr10);
+        refund %= curr10;
 
     }
 }
