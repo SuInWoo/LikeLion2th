cfcab1d2f7005735c1891621c36b3ff2448a60d0
SuInWoo <wbo1026@naver.com>
2022-10-06 00:49:44 +0900

    코드업 1012번: float출력

---
+++ src/codeup/problem1005/Code1012.java
@@ -0,0 +1,11 @@
+package codeup.problem1005;
+
+import java.util.Scanner;
+
+public class Code1012 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float f = sc.nextFloat();
+        System.out.printf("%f",f);
+    }
+}
