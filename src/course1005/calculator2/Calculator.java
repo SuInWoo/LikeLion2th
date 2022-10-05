f42df7de2d976e04bacad3322a04109bf7445d1c
SuInWoo <wbo1026@naver.com>
2022-10-05 14:40:34 +0900

    Calculator 메소드 오버로딩

--- src/course1005/calculator2/Calculator.java
+++ src/course1005/calculator2/Calculator.java
@@ -3,12 +3,21 @@ package course1005.calculator2;
 public class Calculator {
 
     NumberGenerator numberGenerator;
+    int baseNum;
 
     public Calculator(NumberGenerator numberGenerator) {
+
         this.numberGenerator = numberGenerator;
+    }
+
+    public Calculator(NumberGenerator numberGenerator, int baseNum){
+        this.numberGenerator = numberGenerator;
+        this.baseNum = baseNum;
     }
 
     public void plus(){
         System.out.println(10+ numberGenerator.generate(10));
     }
+
+
 }
