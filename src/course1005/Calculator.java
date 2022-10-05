5e0e3edd2b7b7e39303c8929cb7fadbe89ba1927
SuInWoo <wbo1026@naver.com>
2022-10-05 10:16:21 +0900

    plus, minus, multiple, divide 메소드 생성 후 Main클래스에서 실행

---
+++ src/course1005/Calculator.java
@@ -0,0 +1,28 @@
+package course1005;
+
+/**
+ * plus
+ * minus
+ * multiple
+ * divide
+ * 파라미터는 int a,b
+ *
+ * psvm 생성 x
+ */
+public class Calculator {
+    public void plus(int a, int b) {
+        System.out.println(a + b);
+    }
+
+    public void minus(int a, int b) {
+        System.out.println(a - b);
+    }
+
+    public void multiple(int a, int b) {
+        System.out.println(a * b);
+    }
+
+    public void divide(int a, int b) {
+        System.out.println(a / b);
+    }
+}
