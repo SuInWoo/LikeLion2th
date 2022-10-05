3fafadf6bc1d2cea086ebcd776c038c9f5728b55
SuInWoo <wbo1026@naver.com>
2022-10-05 09:28:44 +0900

    인텔리제이 단축키 테스트

--- src/course1005/IntelTest.java
+++ src/course1005/IntelTest.java
@@ -3,5 +3,6 @@ package course1005;
 public class IntelTest {
     public static void main(String[] args) {
         System.out.println("인텔리제이 깃 푸쉬 2차 테스트");
+        System.out.println("단축키 테스트");
     }
 }
