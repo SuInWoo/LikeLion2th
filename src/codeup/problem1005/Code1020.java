e977bee79347a4e0a56110d3c57949c7ec6dd892
SuInWoo <wbo1026@naver.com>
2022-10-06 01:17:29 +0900

    코드업 1020번: 주민번호 입력받아 "-" 제외 후 출력

---
+++ src/codeup/problem1005/Code1020.java
@@ -0,0 +1,13 @@
+package codeup.problem1005;
+
+import java.util.Scanner;
+
+public class Code1020 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] arr = sc.nextLine().split("-");
+
+
+        System.out.printf("%s%s", arr[0], arr[1]);
+    }
+}
