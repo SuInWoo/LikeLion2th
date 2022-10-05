d9924a6b3ae8428879e16e3536a7af2029e0d1c7
SuInWoo <wbo1026@naver.com>
2022-10-06 01:11:19 +0900

    코드업 1018번: 입력받은 수 출력

---
+++ src/codeup/problem1005/Code1018.java
@@ -0,0 +1,11 @@
+package codeup.problem1005;
+
+import java.util.Scanner;
+
+public class Code1018 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println(sc.nextLine());
+    }
+}
