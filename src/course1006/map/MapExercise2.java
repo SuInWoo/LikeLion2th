961d1f4b7eb52cd3bf6d34b1a1aa3fd12a9bb717
SuInWoo <wbo1026@naver.com>
2022-10-06 13:33:42 +0900

    수정 - 기존 알파벳 수정 - OOP사고를 위해 Refactoring 필요

--- src/course1006/map/MapExercise2.java
+++ src/course1006/map/MapExercise2.java
@@ -3,19 +3,24 @@ package course1006.map;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * 알파벳 개수 출력하기
+ *
+ * 1. for문과 if문을 통해 해결
+ * 2. 알파벳 판별 메소드를 통해 refactoring 해보기
+ */
 public class MapExercise2 {
     public static void main(String[] args) {
-        String repoAddr = "https://github.com/SuInWoo/LikeLion2th/tree/main/src";
+        String repoAddr = "https://github.com/SuInWoo/LikeLion2th/tree/main/src".toUpperCase();
+        System.out.println(repoAddr);
 
         HashMap<Character, Integer> alphabetCnt = new HashMap<>();
 
         for (int i = 0; i < repoAddr.length(); i++){
             char c = repoAddr.charAt(i);    //깃 주소를 문자 한개씩 받을 변수
-            int num = (int)c;   //아스키 코드 숫자 비교를 위한 변수
-            if ((num >= 65 && num <= 90) || (num >= 97 && num <= 122)   //대문자와 소문자 모두 담기위해, 처음 들어오는 값이면 value를 1로 초기화
-                    && alphabetCnt.get(c) == null ){
+            if (c >= 'A' && c <= 'Z' && alphabetCnt.get(c) == null ){
                 alphabetCnt.put(c, 1);
-            } else if ((num >= 65 && num <= 90) || (num >= 97 && num <= 122)) { //이미 들어온 값이면 value++
+            } else if (c >= 'A' && c <= 'Z') { //이미 들어온 값이면 value++
                 alphabetCnt.put(c, (alphabetCnt.get(c)+1));
             }
         }
