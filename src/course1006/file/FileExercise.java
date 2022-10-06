193bae93f8b84659c622917615d1a5fe74fb016b
SuInWoo <wbo1026@naver.com>
2022-10-06 15:36:12 +0900

    file에서 글자 출력(한글자, 두글자)

--- src/course1006/file/FileExercise.java
+++ src/course1006/file/FileExercise.java
@@ -1,16 +1,41 @@
 package course1006.file;
 
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 
 public class FileExercise {
-    public static void main(String[] args) throws IOException {
+    public void printFiles(){
         File dir = new File("./");
         File files[] = dir.listFiles();
 
         for (File file: files) {
             System.out.println(file);
         }
+    }
+
+    public char readAChar(String filename) throws IOException{
+        FileReader fileReader = new FileReader("./a_file.txt");
+
+        return (char)fileReader.read();
+    }
+
+    public String read2Char(String filename) throws IOException{
+        FileReader fileReader = new FileReader("./a_file.txt");
+        String str = "";
+        str += (char)fileReader.read();
+        str += (char)fileReader.read();
+
+        return str;
+    }
+    public static void main(String[] args) throws IOException {
+
+        FileExercise fileExercise = new FileExercise();
+        String addr = "./a_file.txt";
+
+        System.out.println(fileExercise.readAChar(addr));
+        System.out.println(fileExercise.read2Char(addr));
+
     }
 
     /**
