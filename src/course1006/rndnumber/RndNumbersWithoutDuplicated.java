1ca298f03c7049ff2226368316a2eb4d5bfd5f1a
SuInWoo <wbo1026@naver.com>
2022-10-06 10:13:30 +0900

    HashSet을 사용하여 랜덤 숫자를 중복 제거하여 출력

--- src/course1006/rndnumber/RndNumbersWithoutDuplicated.java
+++ src/course1006/rndnumber/RndNumbersWithoutDuplicated.java
@@ -1,14 +1,23 @@
 package course1006.rndnumber;
 
+
+import java.util.HashSet;
+import java.util.Set;
+
 public class RndNumbersWithoutDuplicated {
     public static void main(String[] args) {
 
         RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
 
+        Set<Integer> rndNumSet = new HashSet<>();   //중복을 제거한 랜덤 숫자를 담을 Set
+
         for (int i = 0; i < 50; i++) {
             int r = randomNumberGenerator.generate(10);
-            System.out.println(r);
+            System.out.print(r + " ");  //랜덤 숫자 출력
+            rndNumSet.add(r);   //랜덤 숫자를 Set에 담음
         }
+
+        System.out.println("\n" + rndNumSet);
 
     }
 }
