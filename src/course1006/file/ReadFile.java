befa48f41d05fcd62b034e27f64c186a9b3e2311
SuInWoo <wbo1026@naver.com>
2022-10-06 14:25:00 +0900

    txt File 읽기(한 바이트) - BufferedReader 사용

--- src/course1006/file/ReadFile.java
+++ src/course1006/file/ReadFile.java
@@ -2,7 +2,6 @@ package course1006.file;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
