ffda3fea24f2db7c93620ec76360256d31c67a3c
SuInWoo <wbo1026@naver.com>
2022-10-06 14:21:15 +0900

    txt File 읽기 - BufferedReader 사용

---
+++ src/course1006/file/ReadFile.java
@@ -0,0 +1,35 @@
+package course1006.file;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class ReadFile {
+
+    private String fileName;
+
+    public ReadFile(String fileName) {
+        this.fileName = fileName;
+    }
+
+    char readOneByte(String filename) throws IOException {
+
+        try (BufferedReader br = Files.newBufferedReader(Paths.get(filename), StandardCharsets.UTF_8)){
+            return (char)br.read();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        ReadFile readFile = new ReadFile("");
+        char c = readFile.readOneByte("./a_file.txt");
+        System.out.println(c);
+
+    }
+
+}
