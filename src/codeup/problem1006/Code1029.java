22198c1f73abf3d7378a4ceea3758f20172c5cd1
SuInWoo <wbo1026@naver.com>
2022-10-07 08:59:06 +0900

    코드업 1029번: 숫자 출력

---
+++ src/codeup/problem1006/Code1029.java
@@ -0,0 +1,14 @@
+package codeup.problem1006;
+
+
+import java.util.Scanner;
+
+public class Code1029 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double data = sc.nextDouble();
+
+        System.out.println(String.format("%.11f", data));
+    }
+}
