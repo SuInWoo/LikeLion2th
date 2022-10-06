7bdc558d9865396c4fd0a0e249efb7ed412f6067
SuInWoo <wbo1026@naver.com>
2022-10-06 17:23:58 +0900

    코드업 1022번: 공백있는 단어 입력 후 출력

---
+++ src/codeup/problem1006/Code1022.java
@@ -0,0 +1,12 @@
+package codeup.problem1006;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Code1022 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(br.readLine());
+    }
+}
