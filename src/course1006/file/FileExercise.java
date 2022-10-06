493255fc41ba3a86f5b98b6c1b474fce23d77b10
SuInWoo <wbo1026@naver.com>
2022-10-06 15:11:31 +0900

    현재 디렉토리 파일 목록 출력

---
+++ src/course1006/file/FileExercise.java
@@ -0,0 +1,15 @@
+package course1006.file;
+
+import java.io.File;
+import java.io.IOException;
+
+public class FileExercise {
+    public static void main(String[] args) throws IOException {
+        File dir = new File("./");
+        File files[] = dir.listFiles();
+
+        for (File file: files) {
+            System.out.println(file);
+        }
+    }
+}
