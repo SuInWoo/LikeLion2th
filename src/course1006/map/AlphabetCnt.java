f9afa50d3664fd3425d26680ce4964ac319dfc1d
SuInWoo <wbo1026@naver.com>
2022-10-06 13:40:49 +0900

    isAlphabet 메소드 사용 - 강사님 수업 코드

--- src/course1006/map/AlphabetCnt.java
+++ src/course1006/map/AlphabetCnt.java
@@ -1,20 +1,36 @@
 package course1006.map;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class AlphabetCnt {
     public static boolean isAlphabet(char c){
 
-        int num = (int)c;
-        if ((num >= 65 && num <= 90) || (num >= 97 && num <= 122)){
+        if (c >= 'A' && c <= 'Z'){
             return true;
         }else
             return false;
 
     }
-    public static void main(String[] args) {
-        String s = "aabbcabae/";
 
-        for(int i = 0; i < s.length(); i++) {
-            System.out.println(s.charAt(i)+ ":" + isAlphabet(s.charAt(i)));
+    public static void main(String[] args) {
+        String s = "https://github.com/SuInWoo/LikeLion2th/tree/main/src".toUpperCase();
+        HashMap<Character, Integer> alphabetMap = new HashMap<>();
+
+        for(char c = 'A'; c < 'Z'; c++) {
+            alphabetMap.put(c, 0);
+        }
+
+        for (int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            boolean isAlphabet = isAlphabet(c);
+            if (isAlphabet){
+                alphabetMap.put(c, alphabetMap.get(c)+1);
+            }
+        }
+
+        for (Map.Entry<Character,Integer> entry: alphabetMap.entrySet()) {
+            System.out.println(entry.getKey()+" : "+ entry.getValue());
         }
     }
 }
