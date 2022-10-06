ad0838eb4e83de3c8b1db9bbe331946fdef461a2
SuInWoo <wbo1026@naver.com>
2022-10-06 10:35:07 +0900

    랜덤 숫자 알파벳 변환 - HashSet 사용 - 아스키코드 사용

--- src/course1006/rndnumber/RndNumbersWithoutDuplicated.java
+++ src/course1006/rndnumber/RndNumbersWithoutDuplicated.java
@@ -10,7 +10,11 @@ public class RndNumbersWithoutDuplicated {
         RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
 
         Set<Integer> rndNumSet = new HashSet<>();   //중복을 제거한 랜덤 숫자를 담을 Set
+        Set<Character> rndAlphabet = new HashSet<>();
 
+        /**
+         * 랜덤 숫자 받은 후 중복 제거 출력
+         */
         for (int i = 0; i < 50; i++) {
             int r = randomNumberGenerator.generate(10);
             System.out.print(r + " ");  //랜덤 숫자 출력
@@ -18,6 +22,19 @@ public class RndNumbersWithoutDuplicated {
         }
 
         System.out.println("\n" + rndNumSet);
+
+
+        /**
+         * 랜덤 숫자 받은 후 대문자 A~Z로 변환하여 중복 제거 출력
+         */
+
+        for (int i = 0; i < 50; i++){
+            int r = (randomNumberGenerator.generate(26) + 65);
+            char s = (char)r;
+            rndAlphabet.add(s);
+        }
+
+        System.out.println(rndAlphabet);
 
     }
 }
