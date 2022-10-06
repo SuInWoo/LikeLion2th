06dc9918a17597f97db073a248c94a9cc1dc9147
SuInWoo <wbo1026@naver.com>
2022-10-06 10:09:03 +0900

    0~9 랜덤 숫자 출력(인터페이스 사용)

--- src/course1006/rndnumber/RndNumbersWithoutDuplicated.java
+++ src/course1006/rndnumber/RndNumbersWithoutDuplicated.java
@@ -2,12 +2,13 @@ package course1006.rndnumber;
 
 public class RndNumbersWithoutDuplicated {
     public static void main(String[] args) {
+
         RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
+
         for (int i = 0; i < 50; i++) {
             int r = randomNumberGenerator.generate(10);
             System.out.println(r);
         }
-
 
     }
 }
