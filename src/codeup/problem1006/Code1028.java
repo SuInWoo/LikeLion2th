c51d8124960fcb327229ba565f3687635c4cc5c2
SuInWoo <wbo1026@naver.com>
2022-10-06 18:06:02 +0900

    코드업 1028번: 큰 수 입력받아 출력

---
+++ src/codeup/problem1006/Code1028.java
@@ -0,0 +1,15 @@
+package codeup.problem1006;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class Code1028 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BigInteger bigInteger = new BigInteger(br.readLine());
+        System.out.println(bigInteger);
+
+    }
+}
