4815182c0864047b8515168714350d043e19c22a
SuInWoo <wbo1026@naver.com>
2022-10-07 09:01:26 +0900

    코드업 1029번: long 출력

---
+++ src/codeup/problem1006/Code1030.java
@@ -0,0 +1,14 @@
+package codeup.problem1006;
+
+
+import java.util.Scanner;
+
+public class Code1030 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long data = sc.nextLong();
+
+        System.out.println(data);
+    }
+}
