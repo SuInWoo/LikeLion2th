0bdf8988ef402be68008a481aecf4125e09992b0
SuInWoo <wbo1026@naver.com>
2022-10-07 14:27:32 +0900

    전출 전입 cnt결과 파일로 저장

--- src/course1007/PopulationMove.java
+++ src/course1007/PopulationMove.java
@@ -1,13 +1,23 @@
 package course1007;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PopulationMove {
     //전입 to 전출 from
     private int fromSido;
     private int toSido;
+    private String fromSidoKorean;
+    private String toSidoKorean;
+    private Map<Integer, String> sidoMap = new HashMap<>();
 
+    //fromSido, toSido를 받아서 맴버변수에 넣는 기능
     public PopulationMove(int toSido, int fromSido) {
         this.toSido = toSido;
         this.fromSido = fromSido;
+        this.setSidoMap();
+        this.fromSidoKorean = this.sidoMap.get(fromSido);
+        this.toSidoKorean = this.sidoMap.get(toSido);
     }
 
     //오버로딩
@@ -22,5 +32,23 @@ public class PopulationMove {
 
     public int getToSido() {
         return toSido;
+    }
+
+    private void setSidoMap(){
+        sidoMap.put(11,"서울특별시");
+        sidoMap.put(26,"부산광역시");
+        sidoMap.put(27,"대구광역시");
+        sidoMap.put(28,"인천광역시");
+        sidoMap.put(29,"광주광역시");
+        sidoMap.put(30,"대전광역시");
+        sidoMap.put(31,"울산광역시");
+        sidoMap.put(41,"경기도");
+        sidoMap.put(42,"강원도");
+        sidoMap.put(43,"충청북도");
+        sidoMap.put(44,"충청남도");
+        sidoMap.put(45,"전라북도");
+        sidoMap.put(46,"전라남도");
+        sidoMap.put(47,"경상북도");
+        sidoMap.put(50,"제주도");
     }
 }
--- src/course1007/PopulationStatistics.java
+++ src/course1007/PopulationStatistics.java
@@ -4,8 +4,7 @@ import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /*
     파일 읽어오기
@@ -93,20 +92,57 @@ public class PopulationStatistics {
 
     }
 
+    public Map<String, Integer> getMoveCnt(List<PopulationMove> pml){
+        //11,26 이런형식으로 String에 삽입
+        Map<String, Integer> moveCntMap = new HashMap<>();
+        for(PopulationMove pm:pml){
+            String key = pm.getFromSido() + "," + pm.getToSido();
+            if(moveCntMap.get(key) == null){
+                moveCntMap.put(key,1);
+            }else {
+                moveCntMap.put(key, moveCntMap.get(key)+1);
+            }
+        }
+        return moveCntMap;
+    }
+
     public static void main(String[] args) throws IOException {
 
 //        String address = "/Users/suin/Downloads/수업 데이터 파일/2021_인구관련연간자료_20221006_12557.csv";
         String address = "./from_to.txt";
-        PopulationStatistics populationStatistics = new PopulationStatistics();
+        PopulationStatistics ps = new PopulationStatistics();
+        List<PopulationMove> pml = ps.readByLine(address);
 
-        List<PopulationMove> pml = populationStatistics.readByLine(address);
+        Map<String, Integer> map = ps.getMoveCnt(pml);
+        String targetFileName = "each_sido_cnt.txt";
+        ps.createAFile(targetFileName);
+        List<String> cntResult = new ArrayList<>();
+        for (String key : map.keySet()) {
+            String s = String.format("key:%s value:%d \n", key, map.get(key));
+            cntResult.add(s);
+        }
+        ps.write(cntResult,targetFileName);
+
+
+
+
+
+
+
+
+//        Set<Integer> sidoCodes = new HashSet<>();
 
 //        List<String> strings = new ArrayList<>();
-        for (PopulationMove pm : pml){
-            System.out.printf("전입:%s, 전출:%s \n", pm.getFromSido(), pm.getToSido());
+//        for (PopulationMove pm : pml){
+//            System.out.printf("전입:%s, 전출:%s \n", pm.getFromSido(), pm.getToSido());
 //            String fromTo = populationStatistics.fromToString(pm);
 //            strings.add(fromTo);
-        }
+
+//            sidoCodes.add(pm.getFromSido());
+//            sidoCodes.add(pm.getToSido());
+//        }
+//        System.out.println(sidoCodes);
+
 //        System.out.println(pml.size());
 
 //        populationStatistics.createAFile("./from_to.txt");
