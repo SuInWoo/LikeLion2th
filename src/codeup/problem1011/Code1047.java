754bbfad3c4658adb3eaff135080804947c0272d
SuInWoo <wbo1026@naver.com>
2022-10-12 00:55:17 +0900

    코드업 1047번: 시프트연산 예시 int a=10; printf("%d", a<<1); //10을 2배 한 값인 20 이 출력된다. printf("%d", a>>1); //10을 반으로 나눈 값인 5 가 출력된다. printf("%d", a<<2); //10을 4배 한 값인 40 이 출력된다. printf("%d", a>>2); //10을 반으로 나눈 후 다시 반으로 나눈 값인 2 가 출력된다.

---
+++ src/codeup/problem1011/Code1047.java
@@ -0,0 +1,12 @@
+package codeup.problem1011;
+
+import java.util.Scanner;
+
+public class Code1047 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        System.out.printf("%d", num<<1);
+    }
+}
