c50d75f2b058497767e21bdd0d2018f31ae1ddf0
SuInWoo <wbo1026@naver.com>
2022-10-12 10:20:35 +0900

    .

--- src/course1011/NumberDigits.java
+++ src/course1011/NumberDigits.java
@@ -4,22 +4,23 @@ import java.util.Scanner;
 
 public class NumberDigits {
 
-    public int solution(int n){
+    public int solution(int n) {
         int result = 0;
-        while(n > 0) {
+        while (n > 0) {
             result += n % 10;
             n /= 10;
         }
         return result;
     }
+
     public static void main(String[] args) {
 
         NumberDigits numDigits = new NumberDigits();
         int rnt = numDigits.solution(1234);
 
-        if (rnt == 10){
+        if (rnt == 10) {
             System.out.println("테스트 통과");
-        }else {
+        } else {
             System.out.printf("테스트 실패 result:%d \n", rnt);
         }
 
