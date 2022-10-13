81f2b8497c66c100b8064a6e57f7501a425ffd15
SuInWoo <wbo1026@naver.com>
2022-10-13 09:30:23 +0900

    java BubbleSort

--- src/course1013/bublesort/BubbleSort1.java
+++ src/course1013/bublesort/BubbleSort1.java
@@ -4,7 +4,7 @@ public class BubbleSort1 {
     public int[] sort(int[] arr, boolean upAndDown){
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr.length - i - 1; j++) {
-                if (arr[j] > arr[j+1]){
+                if (upAndDown && arr[j] > arr[j+1]){
                     int temp = arr[j+1];
                     arr[j+1] = arr[j];
                     arr[j] = temp;
@@ -17,7 +17,7 @@ public class BubbleSort1 {
         BubbleSort1 bs = new BubbleSort1();
         int[] arr = new int[]{7, 2, 3, 9, 28, 11};
         //오름차순 또는 내림차순으로 정렬하는 알고리즘을 짜보세요.
-        boolean upAndDown = true;  //오름차순: true, 내림차순: false
+//        boolean upAndDown = true;  //오름차순: true, 내림차순: false
         int[] arrUp = bs.sort(arr,true);
 
         for (int i : arrUp) {
