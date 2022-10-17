1e0d3febbafcd589a0f595a69c752d8cfd5dc60e
SuInWoo <wbo1026@naver.com>
2022-10-17 09:26:48 +0900

    RightTriangle - for문

--- src/course1017/triangle/RightTriangle.java
+++ src/course1017/triangle/RightTriangle.java
@@ -1,24 +1,21 @@
 package course1017.triangle;
 
 public class RightTriangle {
-    public static void main(String[] args) {
-        /*
-            *
-            * *
-            * * *
-            * * * *
-            * * * * *
-         */
 
-        //for문 사용
-        int n = 4;  //Scanner 사용해서 추후 입력받을 수 있음
-
+    public void printTriangle(int n){
         for (int i = 1; i <= n; i++) {
             for (int j = 0; j < i; j++) {
                 System.out.print("*");
             }
             System.out.println();
         }
+    }
+    public static void main(String[] args) {
+
+        RightTriangle rt = new RightTriangle();
+        int n = 4;  //Scanner 사용해서 추후 입력받을 수 있음
+
+        rt.printTriangle(n);
 
     }
 }
