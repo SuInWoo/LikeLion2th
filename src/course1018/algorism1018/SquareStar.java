5e2ff191162a20634761ee68b505153e5c9d4b20
SuInWoo <wbo1026@naver.com>
2022-10-18 09:11:03 +0900

    SquareStar - 정사각형 출력

---
+++ src/course1018/algorism1018/SquareStar.java
@@ -0,0 +1,23 @@
+package course1018.algorism1018;
+
+/*
+    정사각형 출력 별찍기
+ */
+public class SquareStar {
+    void printSquare(int n){
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        SquareStar squareStar = new SquareStar();
+        int n = 3;
+
+        squareStar.printSquare(3);
+    }
+
+}
