ea0dd625b671d151286423000b36ff15022c5ed4
SuInWoo <wbo1026@naver.com>
2022-10-20 09:38:38 +0900

    stack

--- src/main/java/course1020/algorism1020/Stack01.java
+++ src/main/java/course1020/algorism1020/Stack01.java
@@ -1,33 +1,46 @@
 package course1020.algorism1020;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Stack01 {
-    private int[] arr = new int[10000];
-    private int pointer = 0;
-
+    private int[] stack;
+    private int pointer = -1;
+    //size 지정 안할 시 10000으로 잡음
     public Stack01() {
+        this.stack = new int[10000];
     }
-
+    //스택 사이즈 직접 지정
     public Stack01(int size) {
-        this.arr = new int[size];
+
+        this.stack = new int[size];
     }
 
-
+    //스택에 값 집어넣기
     public void push(int value) {
-        this.arr[this.pointer] = value;
-        this.pointer++;
+
+        this.stack[++this.pointer] = value;
+    }
+
+    //스택 배열 반환하기
+    public int[] getStack() {
+
+        return stack;
+    }
+
+    //특정 인덱스를 지정해서 값 반환하기
+    public int getArrById(int index){
+        return stack[index];
+    }
+    public int[] getArr(int index){
+        return stack;
     }
 
     public int pop() {
-        // push 10 push 20 point 2
-        int value = this.arr[this.pointer-1];
-        this.pointer--;
-        return value;
+        return stack[pointer--];
     }
+    public boolean isEmpty(){
+        if (pointer == -1) {
+            return true;
+        }
 
-    public int[] getArr() {
-        return arr;
+        return false;
     }
 }
--- src/test/java/course1020/algorism1020/Stack01Test.java
+++ src/test/java/course1020/algorism1020/Stack01Test.java
@@ -1,36 +1,41 @@
 package course1020.algorism1020;
 
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
 
 class Stack01Test {
 
-    @Test
-    void pushTest() {
-        Stack01 stack01 = new Stack01();
-        stack01.push(10);
-        stack01.push(20);
+    @BeforeEach
+    void setup(){
+        // ex) db에서 데이터를 지우는 코드
 
-        int[] arr = stack01.getArr();
-        // 10, 20
-        Assertions.assertEquals(10, arr[0]);
-        Assertions.assertEquals(20, arr[1]);
+        System.out.println("before each");
     }
 
     @Test
-    @DisplayName("push가 잘 되는지 pop이 잘 되는지")
-    void pushAndPop() {
-        Stack01 stack01 = new Stack01();
-        stack01.push(10);
-        stack01.push(20);
+    @DisplayName("push 테스트")
+    void push() {
+        Stack01 stack = new Stack01();
+        stack.push(100);
+        stack.push(200);
 
-        Assertions.assertEquals(20, stack01.pop());
-        Assertions.assertEquals(10, stack01.pop());
+        assertThat(100).isEqualTo(stack.getArrById(0));
+        assertThat(200).isEqualTo(stack.getArrById(1));
+    }
 
-        stack01.push(30);
-        Assertions.assertEquals(30, stack01.pop());
+    @Test
+    @DisplayName("pop 테스트")
+    void pop() {
+        Stack01 stack = new Stack01();
+        int firstInput = 100;
+        int secondInput = 200;
+        stack.push(firstInput);
+        stack.push(secondInput);
+
+        assertThat(secondInput).isEqualTo(stack.pop());
+        assertThat(firstInput).isEqualTo(stack.pop());
     }
 }
