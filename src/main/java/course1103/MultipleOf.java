52d153792de90fcbe42c6436364582adc76934e4
SuInWoo <wbo1026@naver.com>
2022-11-03 10:14:07 +0900

    에라토스테네스의 체 - 2 3 5 7 ... 49 출력 -true인 nums[i]만 출력

--- src/main/java/course1103/MultipleOf.java
+++ src/main/java/course1103/MultipleOf.java
@@ -7,11 +7,20 @@ public class MultipleOf {
 
         //초기 세팅
         int N = 50;
-        int[] nums = new int[N - 1];    //2~50까지 49개
+        int[] nums = new int[N - 1];    //2~50까지 49팅
         boolean[] checks = new boolean[nums.length];    //방문여부를 저장할 배열
         Arrays.fill(checks, true);  //boolean 변수를 초기 true로 세팅
         for (int i = 0; i < nums.length; i++) {
             nums[i] = i + 2;
         }
+
+        for (int i = 2; i < nums.length; i+=2) {
+            checks[i] = false;
+        }
+
+        for (int i = 0; i < checks.length; i++) {
+            if(checks[i]) System.out.println(nums[i]);
+        }
+
     }
 }
