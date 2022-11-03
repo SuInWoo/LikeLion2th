f48069a18368b786292f120bff6b9d48ba96314d
SuInWoo <wbo1026@naver.com>
2022-11-03 09:30:34 +0900

    프로그래머스 소수찾기 - 에라토스테네스의 체 알고리즘

--- src/main/java/course1102/FindPrimeNum.java
+++ src/main/java/course1102/FindPrimeNum.java
@@ -1,5 +1,12 @@
 package course1102;
 
+/*
+    프로그래머스 소수찾기
+
+    1. 리스트로 진행 => n값이 클경우 시간이 너무 오래걸림
+    2. 방문한 값의 idx로 check해줄수있는 boolean[]을 통해 해결 => 배열을 두개 선언해야함
+    3. 방문하면 방문한 배열의 값을 0으로 처리(지워야하는 값만) => 배열 한개로 해결가능
+ */
 public class FindPrimeNum {
     public int solution(int n) {
         int ans = 0;
