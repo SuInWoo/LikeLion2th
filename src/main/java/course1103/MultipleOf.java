123d7e09bb248c416090902e7699884210fd6235
SuInWoo <wbo1026@naver.com>
2022-11-03 09:41:32 +0900

    소수 찾기 - 에라토스테네스의 체 속도 개선 1단계 - %연산 제거 - 2의 배수 출력

--- src/main/java/course1103/MultipleOf.java
+++ src/main/java/course1103/MultipleOf.java
@@ -2,6 +2,7 @@ package course1103;
 
 public class MultipleOf {
     public static void main(String[] args) {
+
         for (int i = 0; (2*i) <= 98; i++) {
             System.out.println(i+1 + ": " + ((i+1)*2));
         }
