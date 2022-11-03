87986d40987991be1c9af596ca28274cabdfe93c
SuInWoo <wbo1026@naver.com>
2022-11-03 10:05:27 +0900

    소수 찾기 - 에라토스테네스의 체 속도 개선 초기세팅

--- src/main/java/course1103/MultipleOf.java
+++ src/main/java/course1103/MultipleOf.java
@@ -1,10 +1,17 @@
 package course1103;
 
+import java.util.Arrays;
+
 public class MultipleOf {
     public static void main(String[] args) {
 
-        for (int i = 0; (2*i) <= 98; i++) {
-            System.out.println(i+1 + ": " + ((i+1)*2));
+        //초기 세팅
+        int N = 50;
+        int[] nums = new int[N - 1];    //2~50까지 49개
+        boolean[] checks = new boolean[nums.length];    //방문여부를 저장할 배열
+        Arrays.fill(checks, true);  //boolean 변수를 초기 true로 세팅
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = i + 2;
         }
     }
 }
