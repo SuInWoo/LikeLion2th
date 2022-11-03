756276d690b6d4aa305788dfbc08745f806e41a5
SuInWoo <wbo1026@naver.com>
2022-11-03 10:50:33 +0900

    에라토스테네스의 체

--- src/main/java/course1103/MultipleOf.java
+++ src/main/java/course1103/MultipleOf.java
@@ -3,24 +3,24 @@ package course1103;
 import java.util.Arrays;
 
 public class MultipleOf {
-    public static void main(String[] args) {
+    public int solution(int N) {
+        if (N == 2) return 1;
+        int[] nums = new int[N-1]; // N-1개만큼 2~50까지 총 49개
+        boolean[] checks = new boolean[nums.length]; // nums만큼 checks생성
+        Arrays.fill(checks, true); // checks를 true로 초기화 cuz 기본값 false
+        for (int i = 0; i < nums.length; i++) nums[i] = i + 2; // 2~49까지 nums에 채우기
 
-        //초기 세팅
-        int N = 50;
-        int[] nums = new int[N - 1];    //2~50까지 49팅
-        boolean[] checks = new boolean[nums.length];    //방문여부를 저장할 배열
-        Arrays.fill(checks, true);  //boolean 변수를 초기 true로 세팅
-        for (int i = 0; i < nums.length; i++) {
-            nums[i] = i + 2;
+        for (int j = 0; j * j <= N; j++) {// N == 49
+            int multipleOf = nums[j]; // j=0 nums[0] = 2
+            // 초항이 2 2n인 2 4 6 8 10
+            for (int i = nums[j] + j; i < nums.length; i+=multipleOf) {
+                checks[i] = false;
+            }
         }
-
-        for (int i = 2; i < nums.length; i+=2) {
-            checks[i] = false;
-        }
-
+        int cnt = 0;
         for (int i = 0; i < checks.length; i++) {
-            if(checks[i]) System.out.println(nums[i]);
+            if(checks[i]) cnt++;
         }
-
+        return cnt;
     }
 }
