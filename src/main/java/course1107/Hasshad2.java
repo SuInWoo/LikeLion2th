0fd762ec27f911be021fa532b148f3fe383746fe
SuInWoo <wbo1026@naver.com>
2022-11-07 09:27:01 +0900

    프로그래머스 하샤드 수 - sumOfDigit

---
+++ src/main/java/course1107/Hasshad2.java
@@ -0,0 +1,26 @@
+package course1107;
+
+public class Hasshad2 {
+    public boolean solution(int x) {
+        boolean ans = true;
+        int sumOfDigit = 0;
+        int num = x;
+
+        while (x > 0) {
+            sumOfDigit += x % 10;
+            x = x /10;
+        }
+
+        if (num % sumOfDigit != 0)
+            ans = false;
+
+        return ans;
+
+    }
+    public static void main(String[] args) {
+        Hasshad2 hasshad2 = new Hasshad2();
+
+        boolean ans = hasshad2.solution(12);
+        System.out.println(ans);
+    }
+}
