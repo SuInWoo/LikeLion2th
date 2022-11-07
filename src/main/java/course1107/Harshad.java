37717a5583ccbbbf1d8b09299c23f14bb91c2658
SuInWoo <wbo1026@naver.com>
2022-11-07 09:33:00 +0900

    프로그래머스 하샤드 수 - String

--- src/main/java/course1107/Harshad.java
+++ src/main/java/course1107/Harshad.java
@@ -7,7 +7,7 @@ package course1107;
     1. 각 자리수의 합을 구함 => 10의 제곱수로 나누어 더함 or String 으로 입력받아 charAt으로 자릿수 더하기(형변환 사용)
     2. 나온 sum을 가지고 기존수에 나누어서 나누어떨어지면 true => %연산 사용
  */
-public class Hasshad {
+public class Harshad {
     public boolean solution(int x) {
         boolean answer = true;
         String str = Integer.toString(x);
@@ -24,9 +24,9 @@ public class Hasshad {
     }
 
     public static void main(String[] args) {
-        Hasshad hasshad = new Hasshad();
+        Harshad harshad = new Harshad();
 
-        boolean ans = hasshad.solution(12);
+        boolean ans = harshad.solution(12);
         System.out.print(ans);
     }
 }
