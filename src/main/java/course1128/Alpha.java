a863bb57364dc60d8d7986f962a2db363550692c
SuInWoo <wbo1026@naver.com>
2022-11-28 09:53:08 +0900

    알파벳 조합 - 한 글자, 두 글자(재귀)

--- src/main/java/course1128/Alpha.java
+++ src/main/java/course1128/Alpha.java
@@ -9,6 +9,14 @@ public class Alpha {
         }
     }
 
+    public static void recurSolution1(char c) {
+
+        if (c > 'Z') return;
+
+        System.out.printf("%c ", c);
+        recurSolution1((char) (c+1));
+    }
+
     // 두 글자 알파벳 출력
     public static void solution2() {
         for (int i = 65; i <= 90; i++) {
@@ -19,11 +27,29 @@ public class Alpha {
         }
     }
 
+    public static void recurSolution2(char c1, char c2) {
+        if (c1 > 'Z')
+            return;
+        else if (c2 > 'Z') {
+            System.out.println();
+            recurSolution2((char) (c1+1), 'A');
+        } else {
+            System.out.printf("%c%c ", c1, c2);
+            recurSolution2(c1, (char) (c2 + 1));
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("한 글자 알파벳");
+        System.out.println("한 글자 알파벳 - for");
         solution1();
         System.out.println();
-        System.out.println("두 글자 알파벳");
+        System.out.println("한 글자 알파벳 - 재귀");
+        recurSolution1('A');
+        System.out.println();
+
+        System.out.println("두 글자 알파벳 - for");
         solution2();
+        System.out.println("두 글자 알파벳 - 재귀");
+        recurSolution2('A', 'A');
     }
 }
