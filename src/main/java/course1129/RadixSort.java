4f139b9c62a5c3308871ba7b3fd4db7f221c2ca0
SuInWoo <wbo1026@naver.com>
2022-11-29 10:30:03 +0900

    RadixSort - 자릿수 중복제거

--- src/main/java/course1129/RadixSort.java
+++ src/main/java/course1129/RadixSort.java
@@ -1,21 +1,37 @@
 package course1129;
 
-import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class RadixSort {
-    public static void main(String[] args) {
-        int[] num = new int[]{1, 0, 9, 5, 4, 7};
-        int[] arr = new int[10];
-        Arrays.fill(arr, -1);
+    public static int[] getDigits(int[] arr) {
+        Set<Integer> set = new HashSet<>();
+        int[] digit = new int[arr.length];
 
-        for (int i = 0; i < num.length; i++) {
-            int n = num[i];
-            arr[n] = n;
+        for (int i = 0; i < arr.length; i++) {  // 자리수
+            if (arr[i] == 0) digit[i] = 1;
+            else digit[i] = (int)(Math.log10(arr[i])+1);
         }
 
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] != -1)
-                System.out.printf("%d ", arr[i]);
+        for (int i : digit) {   // 중복 제거
+            if (!set.contains(i))
+                set.add(i);
+        }
+
+        int[] rnt = new int[set.size()];
+        int idx = 0;
+        for (Integer integer : set) {   // 배열에 담기
+            rnt[idx++] = integer;
+        }
+
+        return rnt;
+    }
+    public static void main(String[] args) {
+        int[] num = new int[]{0, 1, 2, 7, 7, 8, 13, 14, 16, 18, 203};
+
+        int[] result = getDigits(num);
+        for (int i : result) {
+            System.out.printf("%d ", i);
         }
     }
 }
